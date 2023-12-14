673bb711ef9e9c132c4aaf7fac0c1a01be1c1346
glay_luncy <ie925155@gmail.com>
2023-12-15 00:58:39 +0800

    add function comments

--- src/client.cpp
+++ src/client.cpp
@@ -7,28 +7,63 @@ Client::Client(std::string id, const Server& server)
     : server(&server), id(id) {
     crypto::generate_key(public_key, private_key);
 }
-
+/**
+ * Retrieves the ID of the client.
+ *
+ * @return The ID of the client as a string.
+ */
 std::string Client::get_id() {
     return this->id;
 }
-
+/**
+ * Returns the public key of the client.
+ *
+ * @return the public key of the client.
+ */
 std::string Client::get_publickey() const {
     return this->public_key;
 }
-
+/**
+ * Retrieves the wallet balance for the client.
+ *
+ * @return the wallet balance as a double
+ *
+ * @throws ErrorType a description of the error that can be thrown
+ */
 double Client::get_wallet() {
     return server->get_wallet(id);
 }
-
+/**
+ * Signs the given text using the client's private key.
+ *
+ * @param txt the text to be signed
+ *
+ * @return the signed text
+ *
+ * @throws ErrorType if there is an error while signing the text
+ */
 std::string Client::sign(std::string txt) const {
     return crypto::signMessage(private_key, txt);
 }
-
+/**
+ * Transfers money to another client.
+ *
+ * @param receiver the client to transfer money to
+ * @param value the amount of money to transfer
+ *
+ * @return true if the money transfer was successful, false otherwise
+ *
+ * @throws ErrorType if an error occurs during the transfer process
+ */
 bool Client::transfer_money(std::string receiver, double value) const {
     std::string trx = id + "-" + receiver + "-" + std::to_string(value);
     return server->add_pending_trx(trx, sign(trx));
 }
-
+/**
+ * Generates a nonce for the Client class.
+ *
+ * @return The generated nonce.
+ */
 size_t Client::generate_nonce() {
     static std::default_random_engine e;
     static std::uniform_int_distribution u(0, INT_MAX);
--- src/server.cpp
+++ src/server.cpp
@@ -8,7 +8,15 @@
 std::vector<std::string> pending_trxs;
 
 Server::Server() {}
-
+/**
+ * Adds a new client to the server with the specified ID.
+ *
+ * @param id The ID of the client.
+ *
+ * @return A shared pointer to the newly added client.
+ *
+ * @throws None
+ */
 std::shared_ptr<Client> Server::add_client(std::string id) {
 
     for (auto& client : clients) {
@@ -26,7 +34,15 @@ std::shared_ptr<Client> Server::add_client(std::string id) {
     clients[client] = 5;
     return client;
 }
-
+/**
+ * Retrieves a client with the specified ID from the server.
+ *
+ * @param id The ID of the client to retrieve.
+ *
+ * @return A shared pointer to the client with the specified ID, or nullptr if no client is found.
+ *
+ * @throws None
+ */
 std::shared_ptr<Client> Server::get_client(std::string id) const {
     for (auto& client : clients) {
         if (client.first->get_id() == id) {
@@ -35,7 +51,15 @@ std::shared_ptr<Client> Server::get_client(std::string id) const {
     }
     return nullptr;
 }
-
+/**
+ * Gets the wallet balance for a given client ID.
+ *
+ * @param id the ID of the client
+ *
+ * @return the wallet balance of the client
+ *
+ * @throws None
+ */
 double Server::get_wallet(std::string id) const {
     for (auto& client : clients) {
         if (client.first->get_id() == id) {
@@ -44,7 +68,18 @@ double Server::get_wallet(std::string id) const {
     }
     return 0.0;
 }
-
+/**
+ * Parses a transaction string and extracts the sender, receiver, and value.
+ *
+ * @param trx The transaction string to be parsed.
+ * @param sender The variable to store the sender.
+ * @param receiver The variable to store the receiver.
+ * @param value The variable to store the value.
+ *
+ * @return True if the transaction string is successfully parsed, false otherwise.
+ *
+ * @throws std::runtime_error if the transaction string does not match the expected format.
+ */
 bool Server::parse_trx(std::string trx, std::string& sender, std::string& receiver, double& value) {
 	std::regex pattern("(\\w+)-(\\w+)-(\\d+\\.\\d+)");
 	std::smatch results;
@@ -58,7 +93,16 @@ bool Server::parse_trx(std::string trx, std::string& sender, std::string& receiv
 	}
 	return false;
 }
-
+/**
+ * Adds a pending transaction to the server.
+ *
+ * @param trx The transaction string to be added.
+ * @param signature The signature of the transaction.
+ *
+ * @return True if the transaction was successfully added, false otherwise.
+ *
+ * @throws None
+ */
 bool Server::add_pending_trx(std::string trx, std::string signature) const {
     std::string sender{}, receiver{};
     double value;
@@ -77,7 +121,13 @@ bool Server::add_pending_trx(std::string trx, std::string signature) const {
     pending_trxs.push_back(trx);
     return true;
 }
-
+/**
+ * Mines a block on the server.
+ *
+ * @return The nonce value generated for the mined block.
+ *
+ * @throws None
+ */
 size_t Server::mine() {
     std::string mempool{};
     for (auto& trx : pending_trxs) {
@@ -106,7 +156,13 @@ size_t Server::mine() {
     }
     return nonce;
 }
-
+/**
+ * Shows the wallets of all clients connected to the server.
+ *
+ * @param server the server object that contains the list of clients and their wallets
+ *
+ * @throws ErrorType description of error
+ */
 void  show_wallets(const  Server& server)
  {
  	std::cout << std::string(20, '*') << std::endl;
